d838a9693abb1f63c1bab2e587b7748de18d40d6
EmanuelBaigorria <emanueldbaigorria@gmail.com>
2024-03-20 18:49:02 -0300

    asd
    dsad

--- Prac2-GrupoX/src/main/java/com/mycompany/prac2/grupox/Prac2GrupoX.java
+++ Prac2-GrupoX/src/main/java/com/mycompany/prac2/grupox/Prac2GrupoX.java
@@ -12,5 +12,12 @@ public class Prac2GrupoX {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("NICOPUTO");
+        }
+        
+        
+        
     }
 }

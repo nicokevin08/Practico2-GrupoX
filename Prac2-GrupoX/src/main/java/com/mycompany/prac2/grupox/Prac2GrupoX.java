fe767f7c981b1dcb22fd75aeaa2c7412efb2c0b0
Cadelago Nicolas <nicokevin08@hotmail.com>
2024-03-20 18:38:55 -0300

    Subo el Proyecto al Repositorio

---
+++ Prac2-GrupoX/src/main/java/com/mycompany/prac2/grupox/Prac2GrupoX.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prac2.grupox;
+
+/**
+ *
+ * @author nkc
+ */
+public class Prac2GrupoX {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
